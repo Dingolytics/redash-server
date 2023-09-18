1a2166a364fcc089af4634a62db0dfe798c63178
Alexey Kinev <rudy@05bit.com>
2023-09-18 21:17:31 +0400

    Render SVG from parameterized queries with scalar result

--- redash/handlers/widgets.py
+++ redash/handlers/widgets.py
@@ -1,8 +1,57 @@
+from typing import Optional
 from json import loads as json_loads
-from flask import request
+from flask import request, Response
+from jinja2 import Template
 from redash import models
 from redash.handlers.base import routes
 from redash.utils import collect_parameters_from_request
+
+DEFAULT_TEMPLATES = {
+    "plain.svg": Template(
+        '<svg xmlns="http://www.w3.org/2000/svg" '
+            'width="{{ width }}" height="{{ height }}" '
+            'viewBox="0 0 {{ width }} {{ height }}">'
+        '<text x="{{ x }}" y="{{ y }}" fill="{{ color }}" '
+            'text-anchor="{{ anchor }}" '
+            'font-size="{{ size }}" font-family="{{ font }}">'
+        '{{ value }}'
+        '</text>'
+        '</svg>'
+    )
+}
+
+
+
+def template_response(
+    template: str, data: dict, error: Optional[str] = None
+) -> Optional[Response]:
+    # NOTE: We'd want to add custom templates here later.
+    if template not in DEFAULT_TEMPLATES:
+        return None
+
+    if template.endswith(".svg"):
+        try:
+            value = data["rows"][0]["value"]
+        except (KeyError, IndexError):
+            return Response(
+                "Query must return a single row with column named 'value'",
+                mimetype="text/plain",
+                status=422
+            )
+        params = request.args.copy()
+        params.setdefault("width", "100%")
+        params.setdefault("height", 32)
+        params.setdefault("x", 0)
+        params.setdefault("y", 16)
+        params.setdefault("anchor", "start")
+        params.setdefault("color", "black")
+        params.setdefault("size", "1em")
+        params.setdefault("font", "sans-serif")
+        renderer = DEFAULT_TEMPLATES[template]
+        content = renderer.render(value=value, **params)
+        return Response(content, mimetype="image/svg+xml")
+
+    return None
 
 
 @routes.route("/ext/widgets/<int:query_id>/<template>", methods=["GET"])
@@ -26,9 +75,7 @@ def render_widget(query_id: int, template: str):
     else:
         data = None
 
-    # TODO: Render result to template
-
-    return {
+    return template_response(template, data, error) or {
         "data": data,
         "error": error,
     }
